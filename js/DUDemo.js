a59565b3f9d7decee20c06febe4632b1c64a5dc7
Red_Sky4317 <fontlien@gmail.com>
2022-04-12 07:54:57 -0400

    Got some logic to finally start working for appearance change

--- js/DUDemo.js
+++ js/DUDemo.js
@@ -1,55 +1,38 @@
 const character = {
-    gender: 0,
-    head:   0,
-    top:    0,
-    bottom: 0,
-    feet:   0
+    gender: 1,
+    head:   1,
+    top:    1,
+    bottom: 1,
+    feet:   1
 };
 
-const allHeads = ['head_01', 'head_02', 'head_03']
-const allTops = ['top_01', 'top_02', 'top_03']
-const allBottoms = ['bottom_01', 'bottom_02', 'bottom_03']
-const allFeet = ['feet_01', 'feet_02', 'feet_03']
+const allGenders = ['female', 'male'];
+const allHeads = ['head_01', 'head_02', 'head_03'];
+const allTops = ['top_01', 'top_02', 'top_03'];
+const allBottoms = ['bottom_01', 'bottom_02', 'bottom_03'];
+const allFeet = ['feet_01', 'feet_02', 'feet_03'];
 
-// document.getElementById('next-head').onclick = () => {
-//     console.log ('Hello!');
-// }
+const itemCat = [allGenders, allHeads, allTops, allBottoms, allFeet];
 
 const itemSelector = (button) => {
     const buttonType = button.id;
-    // switch (buttonType){
-    //     case 'gender':
-    //         // setItem(itemType)
-    //         break;
-    //     case 'head':
-    //         console.log('I got heads');
-    //         setItem('head', character.head);
-    //         break;
-    //     case '$top':
-    //         console.log('I got top');
-    //         setItem('top', character.top);
-    //         break;
-    //     case 'bottom':
-    //         console.log('I got bottom');
-    //         setItem('bottom', character.bottom);
-    //         break;
-    //     case 'feet':
-    //         console.log('I got feet');
-    //         setItem('feet', character.feet);
-    // }
-    if (buttonType.includes('%head')) {
-        console.log('I got heads');
-        setItem('head', character.head);
-    }
-    if (buttonType.includes('%top')) {
-        console.log('I got top');
-        setItem('top', character.top);
-    }
     console.log(buttonType);
+    if (buttonType.includes('head')) {
+        let url = '../img/head/head_';
+        setItem('head', character.head, url);
+    }
+    if (buttonType.includes('top')) {
+        let url = '../img/top/top_';
+        setItem('top', character.top, url);
+    }
 }
 
-const setItem = (itemType, item) => {
+const setItem = (itemType, item, url) => {
     item++;
-    document.getElementById(itemType).style.background = (`${itemType}%${item}`);
-    console.log(itemType, item);
+    character[itemType] = item;
+    // document.getElementById(itemType).style.background = (`${itemType}%${item}`);
+    itemString = 'url(' + `${url}${item}.png)`;
+    document.getElementById(itemType).style.background = itemString;
+    // document.getElementById(itemType).style.background = "url('../img/head/head_02.png')";
+    console.table(itemType, item, itemString);
 }
