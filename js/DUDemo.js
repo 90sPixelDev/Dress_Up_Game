1a325352c579b850ee55a27dac67c8adf2c88c8f
Red_Sky4317 <fontlien@gmail.com>
2022-04-12 08:11:03 -0400

    Adding extra comments and refactoring a bit to make it easier

--- js/DUDemo.js
+++ js/DUDemo.js
@@ -14,25 +14,36 @@ const allFeet = ['feet_01', 'feet_02', 'feet_03'];
 
 const itemCat = [allGenders, allHeads, allTops, allBottoms, allFeet];
 
+//Passing the button id through function to grab correct item
 const itemSelector = (button) => {
     const buttonType = button.id;
+    //Determining whether going up in array of items or down
+    let isPosDir = buttonType.includes('next') ? true : false;
     console.log(buttonType);
+
+    //Determining which item type to be changing
     if (buttonType.includes('head')) {
         let url = '../img/head/head_';
-        setItem('head', character.head, url);
+        setItem('head', character.head, isPosDir, url);
     }
     if (buttonType.includes('top')) {
         let url = '../img/top/top_';
-        setItem('top', character.top, url);
+        setItem('top', character.top, isPosDir, url);
+    }
+    if (buttonType.includes('bottom')) {
+        let url = '../img/bottom/bottom_';
+        setItem('bottom', character.bottom, isPosDir, url);
+    }
+    if (buttonType.includes('feet')) {
+        let url = '../img/feet/feet_';
+        setItem('feet', character.feet, isPosDir, url);
     }
 }
 
-const setItem = (itemType, item, url) => {
-    item++;
-    character[itemType] = item;
-    // document.getElementById(itemType).style.background = (`${itemType}%${item}`);
-    itemString = 'url(' + `${url}${item}.png)`;
+const setItem = (itemType, itemIndex, isPosDir, url) => {
+    isPosDir ? itemIndex++ : itemIndex--;
+    character[itemType] = itemIndex;
+    itemString = 'url(' + `${url}${itemIndex}.png)`;
     document.getElementById(itemType).style.background = itemString;
-    // document.getElementById(itemType).style.background = "url('../img/head/head_02.png')";
-    console.table(itemType, item, itemString);
+    console.table(itemType, itemIndex, itemString);
 }
