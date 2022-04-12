5f00174d2cce11ff18967f0ca85851bb44904ef9
Red_Sky4317 <lienart.3d@gmail.com>
2022-04-12 06:42:04 -0400

    Added test images to work the logic in game with | Also added possible logic to start the switching of the appearance of the character

--- js/DUDemo.js
+++ js/DUDemo.js
@@ -1,65 +1,55 @@
-const characters = [{
-    name:   'Nangfah',
-    gender: 'Female',
-    age:    5,
-    rank:   'A+'
-},
-{
-    name:   'Menelik',
-    gender: 'Male',
-    age:    5, 
-    rank:   'A+'
-},
-{
-    name:   'Joha',
-    gender: 'Male',
-    age:    7,
-    rank:   'B'
-},
-{
-    name:   'Kira',
-    gender: 'Female',
-    age:    4,
-    rank:   'C'
-},
-{
-    name:   'Tozan',
-    gender: 'Male',
-    age:    6,
-    rank:   'B+'
-},
-{
-    name:   'Filia',
-    gender: 'Female',
-    age:    8,
-    rank:   'A'
-}]
+const character = {
+    gender: 0,
+    head:   0,
+    top:    0,
+    bottom: 0,
+    feet:   0
+};
 
-const yToOCharas = characters.sort((a, b) => a.age - b.age)
+const allHeads = ['head_01', 'head_02', 'head_03']
+const allTops = ['top_01', 'top_02', 'top_03']
+const allBottoms = ['bottom_01', 'bottom_02', 'bottom_03']
+const allFeet = ['feet_01', 'feet_02', 'feet_03']
 
-const descRank = characters.sort((a, b) => {
-    if (a.rank < b.rank) return -1;
-    if (a.rank > b.rank) return 1;
-    return 0;
-})
+// document.getElementById('next-head').onclick = () => {
+//     console.log ('Hello!');
+// }
 
-const ascRank = characters.sort((a, b) => {
-  if (b.rank < a.rank) return -1;
-  if (b.rank >a.rank) return 1;
-  return 0;  
-})
+const itemSelector = (button) => {
+    const buttonType = button.id;
+    // switch (buttonType){
+    //     case 'gender':
+    //         // setItem(itemType)
+    //         break;
+    //     case 'head':
+    //         console.log('I got heads');
+    //         setItem('head', character.head);
+    //         break;
+    //     case '$top':
+    //         console.log('I got top');
+    //         setItem('top', character.top);
+    //         break;
+    //     case 'bottom':
+    //         console.log('I got bottom');
+    //         setItem('bottom', character.bottom);
+    //         break;
+    //     case 'feet':
+    //         console.log('I got feet');
+    //         setItem('feet', character.feet);
+    // }
+    if (buttonType.includes('%head')) {
+        console.log('I got heads');
+        setItem('head', character.head);
+    }
+    if (buttonType.includes('%top')) {
+        console.log('I got top');
+        setItem('top', character.top);
+    }
+    console.log(buttonType);
+}
 
-const charaGenderCount = characters.reduce((groupedGender, chara) => {
-    const charaGen = chara.gender;
-    if (!groupedGender[charaGen]) groupedGender[charaGen] = [];
-    groupedGender[charaGen].push(chara)
-    return groupedGender;
-}, {})
-
-const charaAgeCount = characters.reduce((groupedAge, chara) => {
-    const charaAge = chara.age;
-    if (!groupedAge[charaAge]) groupedAge[charaAge] = [];
-    groupedAge[charaAge].push(chara)
-    return groupedAge;
-}, {})
-
+const setItem = (itemType, item) => {
+    item++;
+    document.getElementById(itemType).style.background = (`${itemType}%${item}`);
+    console.log(itemType, item)
+}
